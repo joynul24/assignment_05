ff788c83cc552201d646a2dc67f26a8181ebee16
joynul24 <joynula919@gmail.com>
2024-12-08 03:17:46 +0600

    After completing the addition and subtraction, validation case of a card, I added

--- js/home.js
+++ js/home.js
@@ -1,7 +1,32 @@
 // first card events
 document.getElementById('btn-donate-1').addEventListener('click', function () {
+
     const inputAmount = getInputFieldValueById('input-card-1');
     
-    const cardAmount = getTextFieldValueById('balance-card-1');
-    console.log(cardAmount);
+    // validation
+    if (isNaN(inputAmount)) {
+        alert("Please input number type!");
+        return;
+    } else if (inputAmount < 0) {
+        alert("Please type positive number!")
+        return;
+    }
+
+    const cardBalance = getTextFieldValueById('balance-card-1');
+
+    const availableBalance = getTextFieldValueById('available-balance');
+
+    const updateCardBalance = inputAmount + cardBalance;
+    const updateAvailableBalance = availableBalance - inputAmount;
+
+    document.getElementById('available-balance').innerText = updateAvailableBalance;
+
+    document.getElementById('balance-card-1').innerText = updateCardBalance;
+    document.getElementById('input-card-1').value = '';
 });
+
+
+
+
+// first card events
+// document.getElementById('')
